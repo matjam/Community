8574f9b0fc562a0bea117e01805ba497a3ce27fb
Nathan Ollerenshaw <chrome@stupendous.net>
2011-10-08 19:21:48 +1100

    Initial Commit

---
+++ src/net/stupendous/community/CommunityPlugin.java
@@ -0,0 +1,19 @@
+package net.stupendous.community;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommunityPlugin extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
